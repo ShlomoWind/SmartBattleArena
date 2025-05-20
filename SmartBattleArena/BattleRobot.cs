35b40e105a4e51681f6a1952919989cfb33f9dfa
Shlomo <sh.z.wind@gmail.com>
2025-05-20 15:38:42 +0300

    class BattleRobot IPowerable

---
+++ SmartBattleArena/BattleRobot.cs
@@ -0,0 +1,14 @@
+﻿using SmartBattleArena;
+
+public class BattleRobot : IPowerable, IMovable, IAttackable
+{
+    public bool IsOn { get; set; } = false;
+    public void TurnOn()
+    {
+        IsOn = true;
+    }
+    public void TurnOff()
+    {
+        IsOn = false;
+    }
+}
